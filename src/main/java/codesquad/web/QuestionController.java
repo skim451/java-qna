722ca0d43e8ae61cc9709ae9443c1669008d3ca3
Sehwan Kim <skim451@illinois.edu>
2018-01-31 16:21:44 +0900

    Added Question Service
    - QuestionController now has a depedency on QuestionService instead of
      on QuestionRepository.

--- src/main/java/codesquad/web/QuestionController.java
+++ src/main/java/codesquad/web/QuestionController.java
@@ -1,24 +1,25 @@
 package codesquad.web;
 
 import codesquad.domain.Question;
-import codesquad.domain.QuestionRepository;
 import codesquad.domain.User;
-import codesquad.security.HttpSessionUtils;
+import codesquad.etc.CannotDeleteException;
 import codesquad.security.LoginUser;
+import codesquad.service.QnaService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/questions")
 public class QuestionController {
 
+    private static final Logger log = LoggerFactory.getLogger(QuestionController.class);
+
     @Autowired
-    private QuestionRepository questionRepository;
+    private QnaService qnaService;
 
     @GetMapping("/form")
     public String getQuestionForm() {
@@ -35,16 +36,26 @@ public class QuestionController {
                 .setTitle(title)
                 .setWriter(loginUser);
 
-        questionRepository.save(question);
+        qnaService.create(loginUser, question);
 
         return "redirect:/";
     }
 
     @GetMapping("/{id}")
     public String showQuestionDetail(@PathVariable long id, Model model) {
-        Question question = questionRepository.findOne(id);
+        Question question = qnaService.findById(id);
         model.addAttribute("question", question);
 
         return "qna/show";
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteQuestion(@LoginUser User loginUser, @PathVariable long id) {
+        try {
+            qnaService.deleteQuestion(loginUser, id);
+        } catch (CannotDeleteException e) {
+            log.debug(e.getMessage());
+        }
+        return "redirect:/";
     }
 }
