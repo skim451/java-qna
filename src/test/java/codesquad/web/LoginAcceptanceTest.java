caa8b4bf2935ccc7701134107120fa54838c6f1a
Sehwan Kim <skim451@illinois.edu>
2018-01-31 14:10:17 +0900

    Added loginTester

--- src/test/java/codesquad/web/LoginAcceptanceTest.java
+++ src/test/java/codesquad/web/LoginAcceptanceTest.java
@@ -5,14 +5,10 @@ import codesquad.etc.HtmlFormDataBuilder;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.*;
-import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import support.test.AcceptanceTest;
 
-import java.util.Arrays;
-
 import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertThat;
 
 public class LoginAcceptanceTest extends AcceptanceTest {
@@ -20,20 +16,45 @@ public class LoginAcceptanceTest extends AcceptanceTest {
     private UserRepository userRepository;
 
     @Test
-    public void create() throws Exception {
+    public void loginSuccessTest() throws Exception {
         HtmlFormDataBuilder headersBuilder = HtmlFormDataBuilder.urlEncodedForm();
 
-        String userId = "testuser";
+        String userId = "javajigi";
         headersBuilder.addParameter("userId", userId)
-                .addParameter("password", "password")
-                .addParameter("name", "자바지기")
-                .addParameter("email", "javajigi@slipp.net");
+                .addParameter("password", "test");
         HttpEntity<MultiValueMap<String, Object>> request = headersBuilder.build();
 
-        ResponseEntity<String> response = template().postForEntity("/users", request, String.class);
+        ResponseEntity<String> response = template().postForEntity("/login", request, String.class);
 
         assertThat(response.getStatusCode(), is(HttpStatus.FOUND));
-        assertNotNull(userRepository.findByUserId(userId));
         assertThat(response.getHeaders().getLocation().getPath(), is("/users"));
+    }
+
+    @Test
+    public void loginFailTest_invalid_pw() throws Exception {
+        HtmlFormDataBuilder headersBuilder = HtmlFormDataBuilder.urlEncodedForm();
+
+        String userId = "javajigi";
+        headersBuilder.addParameter("userId", userId)
+                .addParameter("password", "test1");
+        HttpEntity<MultiValueMap<String, Object>> request = headersBuilder.build();
+
+        ResponseEntity<String> response = template().postForEntity("/login", request, String.class);
+
+        assertThat(response.getStatusCode(), is(HttpStatus.OK));
+    }
+
+    @Test
+    public void loginFailTest_invalid_id() throws Exception {
+        HtmlFormDataBuilder headersBuilder = HtmlFormDataBuilder.urlEncodedForm();
+
+        String userId = "javajigi1";
+        headersBuilder.addParameter("userId", userId)
+                .addParameter("password", "test");
+        HttpEntity<MultiValueMap<String, Object>> request = headersBuilder.build();
+
+        ResponseEntity<String> response = template().postForEntity("/login", request, String.class);
+
+        assertThat(response.getStatusCode(), is(HttpStatus.OK));
     }
 }
